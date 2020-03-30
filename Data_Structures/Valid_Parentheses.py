9ceeeaa120191af9cfd461270cec47e65033f673
ahmedabdelhameed <ahmed_barbary@yahoo.com>
2020-03-30 00:54:36 +0000

    Second Copy

--- Data_Structures/Valid_Parentheses.py
+++ Data_Structures/Valid_Parentheses.py
@@ -1,3 +1,10 @@
+#Given a string containing just the characters '(', ')',# '{', '}', '[' and ']', determine if the input string is valid.
+
+#An input string is valid if:
+
+#Open brackets must be closed by the same type of brackets.
+#Open brackets must be closed in the correct order.
+
 def isValid(s: str) -> bool:
    d = {'{':'}', '[': ']','(':')'}
    if s == []:
@@ -14,5 +21,4 @@ def isValid(s: str) -> bool:
         stack.append(s[i])
    if stack ==[]:
      return True
-
 print(isValid(''))
