5cd0a65a2df840939550ee7cf811d19567dd3ae7
ahmedabdelhameed <ahmed_barbary@yahoo.com>
2020-03-30 20:29:48 +0000

    Version2

---
+++ Data_Structures/Move-Zeroes-V2.py
@@ -0,0 +1,25 @@
+#Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+#Example:
+
+#Input: [0,1,0,3,12]
+#Output: [1,3,12,0,0]
+
+
+from typing import List
+
+def moveZeroes(nums: List[int]) -> None:
+    j = 0
+    for i in range(0, len(nums)):
+      if nums[i] != 0:
+        nums[j] = nums[i]
+        nums[i] = 0
+        j +=1
+      
+
+#Sample run
+mylist= [0,1,0,3,1,2]
+moveZeroes(mylist)
+print(mylist)
+
+        
